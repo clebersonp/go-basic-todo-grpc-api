4f380a560b85268ed928b7beb222704ca6236306
Cleberson <pauluci.cleberson@gmail.com>
2024-02-24 15:44:04 -0300

    Refactored main go file, call other functions and code organized

--- main.go
+++ main.go
@@ -1,106 +1,38 @@
 package main
 
 import (
-	"context"
+	"flag"
 	"fmt"
+	"github.com/clebersonp/go-basic-todo-grpc-api/middleware"
 	"github.com/clebersonp/go-basic-todo-grpc-api/model"
-	"github.com/google/uuid"
+	"github.com/clebersonp/go-basic-todo-grpc-api/proto"
 	"google.golang.org/grpc"
-	"google.golang.org/grpc/codes"
-	"google.golang.org/grpc/metadata"
-	"google.golang.org/grpc/status"
 	"log"
 	"net"
-	"strings"
 )
 
 var (
-	errMissingMetadata = status.Error(codes.InvalidArgument, "missing metadata")
-	errInvalidToken    = status.Error(codes.Unauthenticated, "invalid token")
+	port    = flag.Int("port", 8089, "Api port number")
+	network = flag.String("network", "tcp", "Kind of networks")
 )
 
-type taskerServer struct {
-	model.UnimplementedTaskerServer
-}
-
-func (t *taskerServer) Create(ctx context.Context, req *model.CreateRequest) (*model.CreateResponse, error) {
-	log.Println("service:", t)
-	log.Println("UnimplementedTaskerServer:", t.UnimplementedTaskerServer)
-	log.Println("context:", ctx)
-
-	// get metadata from request (like headers)
-	md, ok := metadata.FromIncomingContext(ctx)
-	if ok {
-		log.Println("MD:", md)
-		log.Println("Authorization:", md["authorization"])
-	}
-	log.Println("req:", req)
-	log.Println("Todo", req.GetTodo())
-
-	// sending metadata to client
-	reqId := uuid.New().String()
-	header := metadata.Pairs("x-request-id", reqId)
-	err := grpc.SendHeader(ctx, header)
-	if err != nil {
-		return &model.CreateResponse{
-			Error:       true,
-			Description: fmt.Sprintf("Error to add header info: %q", err),
-		}, err
-	}
-
-	trailer := metadata.Pairs("some-key", "some-value")
-	err = grpc.SetTrailer(ctx, trailer)
-	if err != nil {
-		return &model.CreateResponse{
-			Error:       true,
-			Description: fmt.Sprintf("Error to add trailer info: %q", err),
-		}, err
-	}
-
-	resp := &model.CreateResponse{
-		Error:       false,
-		Description: "Todo created.",
-	}
-	return resp, nil
-}
-
 func main() {
-	listen, err := net.Listen("tcp", ":8089")
+	flag.Parse()
+	listen, err := net.Listen(*network, fmt.Sprintf(":%d", *port))
 	if err != nil {
 		log.Fatalf("cannot create a tcp listener: %q", err)
 	}
 
 	opts := []grpc.ServerOption{
-		grpc.UnaryInterceptor(ensureValidToken),
+		grpc.ChainUnaryInterceptor(middleware.RequestID, middleware.EnsureValidToken),
 	}
 
 	serverRegistrar := grpc.NewServer(opts...)
-	model.RegisterTaskerServer(serverRegistrar, &taskerServer{})
+	proto.RegisterTaskerServer(serverRegistrar, &model.TaskerServer{})
 
-	log.Println("Listen:", listen.Addr())
+	log.Println("Listen at:", listen.Addr())
 	err = serverRegistrar.Serve(listen)
 	if err != nil {
 		log.Fatalf("Impossible to start up the server: %q", err)
 	}
-}
-
-func ensureValidToken(ctx context.Context, req any, info *grpc.UnaryServerInfo, handler grpc.UnaryHandler) (resp any, err error) {
-	md, ok := metadata.FromIncomingContext(ctx)
-	if !ok {
-		return nil, errMissingMetadata
-	}
-
-	if !valid(md["authorization"]) {
-		return nil, errInvalidToken
-	}
-
-	return handler(ctx, req)
-}
-
-func valid(authorization []string) bool {
-	if len(authorization) < 1 {
-		return false
-	}
-	token := strings.TrimPrefix(authorization[0], "Bearer ")
-	return token == "some-secret-token"
 }
