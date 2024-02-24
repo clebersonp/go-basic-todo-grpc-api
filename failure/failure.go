1ee890747ce21fb89499f1bcd22b156abfeb7e6c
Cleberson <pauluci.cleberson@gmail.com>
2024-02-24 16:59:41 -0300

    Created new error messages

--- failure/failure.go
+++ failure/failure.go
@@ -8,4 +8,7 @@ import (
 var (
 	ErrMissingMetadata = status.Error(codes.InvalidArgument, "missing metadata")
 	ErrInvalidToken    = status.Error(codes.Unauthenticated, "invalid token")
+	ErrInvalidTodoID   = status.Error(codes.InvalidArgument, "invalid todo id")
+	ErrTodoIdRequired  = status.Error(codes.InvalidArgument, "todo id is required")
+	ErrTodoNotFound    = status.Error(codes.NotFound, "todo not found")
 )
