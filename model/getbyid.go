c4924104bdff1a688e3912eaf9fc8f70c32975f4
Cleberson <pauluci.cleberson@gmail.com>
2024-02-24 17:05:38 -0300

    Implemented getById service

---
+++ model/getbyid.go
@@ -0,0 +1,40 @@
+package model
+
+import (
+	"context"
+	"github.com/clebersonp/go-basic-todo-grpc-api/failure"
+	"github.com/clebersonp/go-basic-todo-grpc-api/header"
+	"github.com/clebersonp/go-basic-todo-grpc-api/proto"
+	"github.com/google/uuid"
+	"log"
+)
+
+func (t *TaskerServer) GetByID(ctx context.Context, req *proto.GetRequest) (*proto.Todo, error) {
+	reqId := header.GetRequestID(ctx)
+	todoID := req.GetId()
+	log.Printf("%s: %s, TodoID: %s\n", header.XRequestID, reqId, todoID)
+
+	err := validateId(todoID)
+	if err != nil {
+		log.Println(err)
+		return nil, err
+	}
+
+	todo, ok := get(todoID)
+	if !ok {
+		e := failure.ErrTodoNotFound
+		log.Println(e)
+		return nil, e
+	}
+	return todo, nil
+}
+
+func validateId(id string) error {
+	if id == "" {
+		return failure.ErrTodoIdRequired
+	}
+	if _, err := uuid.Parse(id); err != nil {
+		return failure.ErrInvalidTodoID
+	}
+	return nil
+}
