78d49e5628e03955290a724592f269e0206c2367
Cleberson <pauluci.cleberson@gmail.com>
2024-02-24 15:39:29 -0300

    Created header package

---
+++ header/header.go
@@ -0,0 +1,20 @@
+package header
+
+import (
+	"context"
+	"google.golang.org/grpc/metadata"
+)
+
+const XRequestID = "x-request-id"
+
+func GetRequestID(ctx context.Context) string {
+	var reqId string
+	mdOut, ok := metadata.FromOutgoingContext(ctx)
+	if ok {
+		values := mdOut.Get(XRequestID)
+		if len(values) >= 1 {
+			reqId = values[0]
+		}
+	}
+	return reqId
+}
