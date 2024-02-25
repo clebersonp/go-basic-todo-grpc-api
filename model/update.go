e9e4f454e93f4612baf6b4ecf7a9b661ad3b4ac2
Cleberson <pauluci.cleberson@gmail.com>
2024-02-24 21:57:54 -0300

    Created update todo service

---
+++ model/update.go
@@ -0,0 +1,22 @@
+package model
+
+import (
+	"context"
+	pb "github.com/clebersonp/go-basic-todo-grpc-api/proto"
+	"google.golang.org/protobuf/types/known/emptypb"
+	"log"
+)
+
+func (t *TaskerServer) Update(_ context.Context, req *pb.TodoUpdateRequest) (*emptypb.Empty, error) {
+	todo := &pb.Todo{
+		Id:          req.Id,
+		Title:       req.Title,
+		Description: req.Description,
+		Status:      req.Status,
+	}
+	if err := update(todo); err != nil {
+		log.Println(err)
+		return nil, err
+	}
+	return &emptypb.Empty{}, nil
+}
