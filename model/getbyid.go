1f687e6b3c20aca6f51a35ff8f1d59796f792ab7
Cleberson <pauluci.cleberson@gmail.com>
2024-02-24 21:23:06 -0300

    Refactored getById service, moved the validation todo Id to validation package

--- model/getbyid.go
+++ model/getbyid.go
@@ -5,36 +5,26 @@ import (
 	"github.com/clebersonp/go-basic-todo-grpc-api/failure"
 	"github.com/clebersonp/go-basic-todo-grpc-api/header"
 	pb "github.com/clebersonp/go-basic-todo-grpc-api/proto"
-	"github.com/google/uuid"
+	"github.com/clebersonp/go-basic-todo-grpc-api/validation"
 	"log"
 )
 
-func (t *TaskerServer) GetByID(ctx context.Context, req *pb.GetRequest) (*pb.GetByIdResponse, error) {
+func (t *TaskerServer) GetByID(ctx context.Context, req *pb.TodoByIdRequest) (*pb.GetByIdResponse, error) {
 	reqId := header.GetRequestID(ctx)
 	todoID := req.GetId()
 	log.Printf("%s: %s, TodoID: %s\n", header.XRequestID, reqId, todoID)
 
-	err := validateId(todoID)
+	err := validation.TodoId(todoID)
 	if err != nil {
 		log.Println(err)
 		return nil, err
 	}
 
-	todo, ok := get(todoID)
+	todo, ok := getById(todoID)
 	if !ok {
 		e := failure.ErrTodoNotFound
 		log.Println(e)
 		return nil, e
 	}
 	return &pb.GetByIdResponse{Todo: todo}, nil
-}
-
-func validateId(id string) error {
-	if id == "" {
-		return failure.ErrTodoIdRequired
-	}
-	if _, err := uuid.Parse(id); err != nil {
-		return failure.ErrInvalidTodoID
-	}
-	return nil
 }
