e5f28cabe0a41b7606643c8db8d7c923c67b043b
Cleberson <pauluci.cleberson@gmail.com>
2024-03-02 00:37:21 -0300

    Created new variables for user operations

--- failure/failure.go
+++ failure/failure.go
@@ -6,10 +6,12 @@ import (
 )
 
 var (
-	ErrMissingMetadata = status.Error(codes.InvalidArgument, "missing metadata")
-	ErrInvalidToken    = status.Error(codes.Unauthenticated, "invalid token")
-	ErrInvalidTodoID   = status.Error(codes.InvalidArgument, "invalid todo id")
-	ErrTodoIdRequired  = status.Error(codes.InvalidArgument, "todo id is required")
-	ErrTodoRequired    = status.Error(codes.InvalidArgument, "todo is required")
-	ErrTodoNotFound    = status.Error(codes.NotFound, "todo not found")
+	ErrMissingMetadata        = status.Error(codes.InvalidArgument, "missing metadata")
+	ErrInvalidToken           = status.Error(codes.Unauthenticated, "invalid token")
+	ErrInvalidTodoID          = status.Error(codes.InvalidArgument, "invalid todo id")
+	ErrTodoIdRequired         = status.Error(codes.InvalidArgument, "todo id is required")
+	ErrTodoRequired           = status.Error(codes.InvalidArgument, "todo is required")
+	ErrTodoNotFound           = status.Error(codes.NotFound, "todo not found")
+	ErrEmailPasswordIncorrect = status.Error(codes.Unauthenticated, "your email or password is incorrect")
+	ErrUserAlreadyExists      = status.Error(codes.AlreadyExists, "a user with that email address already exists")
 )
