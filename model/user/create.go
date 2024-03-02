76a836a146de680bcb06cba7c5c00c72ba72c427
Cleberson <pauluci.cleberson@gmail.com>
2024-03-02 00:39:38 -0300

    Implemented endpoint signup of user

--- model/user/create.go
+++ model/user/create.go
@@ -2,15 +2,40 @@ package user
 
 import (
 	"context"
+	"github.com/clebersonp/go-basic-todo-grpc-api/header"
 	pb "github.com/clebersonp/go-basic-todo-grpc-api/proto/user"
+	"github.com/google/uuid"
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/metadata"
 	"google.golang.org/protobuf/types/known/emptypb"
+	"google.golang.org/protobuf/types/known/timestamppb"
 	"log"
 )
 
-func (s *ServerUser) SignUp(_ context.Context, req *pb.CreateUserRequest) (*emptypb.Empty, error) {
+func (s *ServerUser) SignUp(ctx context.Context, req *pb.CreateUserRequest) (*emptypb.Empty, error) {
+	reqId := header.GetRequestID(ctx)
+	log.Printf("User %v for X-Request-ID %q will be created", req, reqId)
 
-	// TODO must be implement
-	log.Println("SignUp Unimplemented")
+	newUser := &pb.User{
+		Id:        uuid.New().String(),
+		Name:      req.Name,
+		Email:     req.Email,
+		Password:  req.Password,
+		CreatedAt: timestamppb.Now(),
+	}
+
+	err := add(newUser)
+	if err != nil {
+		log.Println(err)
+		return nil, err
+	}
+
+	md := metadata.Pairs(header.UserId, newUser.Id)
+	err = grpc.SetTrailer(ctx, md)
+	if err != nil {
+		log.Println(err)
+		return nil, err
+	}
 
 	return &emptypb.Empty{}, nil
 }
