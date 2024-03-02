f95644f7a0ad664a5cefb45e21e30739defbb7df
Cleberson <pauluci.cleberson@gmail.com>
2024-03-02 00:40:11 -0300

    Implemented signin endpoint of user

--- model/user/login.go
+++ model/user/login.go
@@ -2,17 +2,25 @@ package user
 
 import (
 	"context"
+	"github.com/clebersonp/go-basic-todo-grpc-api/header"
 	pb "github.com/clebersonp/go-basic-todo-grpc-api/proto/user"
 	"log"
 )
 
-func (s *ServerUser) SignIn(_ context.Context, req *pb.LoginUserRequest) (*pb.LoginUserResponse, error) {
+func (s *ServerUser) SignIn(ctx context.Context, req *pb.LoginUserRequest) (*pb.LoginUserResponse, error) {
+	reqId := header.GetRequestID(ctx)
+	userEmail := req.GetEmail()
+	log.Printf("%s: %s, User_Email: %s\n", header.XRequestID, reqId, userEmail)
 
-	// TODO must implement
-	log.Println("SignIn Unimplemented")
+	// TODO must implements token
+	user, err := authentication(req.Email, req.Password)
+	if err != nil {
+		log.Println(err)
+		return nil, err
+	}
 
 	return &pb.LoginUserResponse{
-		Token:     "Unimplemented",
+		Token:     user.Id,
 		ExpiresIn: 0,
 	}, nil
 }
